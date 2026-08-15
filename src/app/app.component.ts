aee03ecfe70d594b83f335ed26fa43a292c7510a
Shrinath999 <shrinathdeshpande894@gmail.com>
2026-08-15 21:06:40 +0530

    Initial portfolio project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import{AboutComponent} from './components/about/about.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ExperienceComponent } from "./components/experience/experience.component";
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CertificationsComponent } from './components/certifications/certifications.component';
+import { ContactComponent } from "./components/contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, HomeComponent, AboutComponent, SkillsComponent, ExperienceComponent, ProjectsComponent, CertificationsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'My-Portfolio';
+  title = 'Portfolio';
 }
---
+++ src/app/components/azure/azure.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-azure',
+  standalone: true,
+  imports: [],
+  templateUrl: './azure.component.html',
+  styleUrl: './azure.component.css'
+})
+export class AzureComponent {
+
+}
